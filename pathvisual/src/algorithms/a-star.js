211eb44a3e6833467e81326db318cad8136e5fb4
Shihab Islam <shihabhunterislam@gmail.com>
2022-10-31 20:43:40 -0400

    finished a-star algortihm tracker.

--- pathvisual/src/algorithms/a-star.js
+++ pathvisual/src/algorithms/a-star.js
@@ -42,4 +42,16 @@ export function aStarGetNeighbors(node, grid) {
     const neighbors = [];
     const { row, col } = node;
     if (row > 0) {
-}
+      neighbors.push(grid[row - 1][col]);
+    }
+    if (col < grid[0].length - 1) {
+      neighbors.push(grid[row][col + 1]);
+    }
+    if (row < grid.length - 1) {
+      neighbors.push(grid[row + 1][col]);
+    }
+    if (col > 0) {
+      neighbors.push(grid[row][col - 1]);
+    }
+    return neighbors;
+  }
