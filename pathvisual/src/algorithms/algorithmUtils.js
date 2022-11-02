53a2a1a579ebc96a643c217d80f6e56ee078ca37
Shihab Islam <shihabhunterislam@gmail.com>
2022-11-01 20:35:22 -0400

    visit nearby nodes on position base.

--- pathvisual/src/algorithms/algorithmUtils.js
+++ pathvisual/src/algorithms/algorithmUtils.js
@@ -20,3 +20,11 @@ export function getAllNodes(grid) {
     if (col < grid[0].length - 1) {
       neighbors.push(grid[row][col + 1]);
     }
+    if (row < grid.length - 1) {
+        neighbors.push(grid[row + 1][col]);
+      }
+      if (col > 0) {
+        neighbors.push(grid[row][col - 1]);
+      }
+      return neighbors.filter((neighbor) => !neighbor.isVisited);
+    }
