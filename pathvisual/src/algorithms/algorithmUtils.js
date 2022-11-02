40a543f8219c2395c81c82d157ecd172b7c69b6e
Shihab Islam <shihabhunterislam@gmail.com>
2022-11-01 20:34:29 -0400

    establish movement paths.

--- pathvisual/src/algorithms/algorithmUtils.js
+++ pathvisual/src/algorithms/algorithmUtils.js
@@ -11,3 +11,12 @@ export function getAllNodes(grid) {
   export function sortedNodes(unvisitedNodes) {
     return unvisitedNodes.sort((a, b) => a.distance - b.distance);
   }
+  export function getNeighbors(node, grid) {
+    const neighbors = [];
+    const { row, col } = node;
+    if (row > 0) {
+      neighbors.push(grid[row - 1][col]);
+    }
+    if (col < grid[0].length - 1) {
+      neighbors.push(grid[row][col + 1]);
+    }
