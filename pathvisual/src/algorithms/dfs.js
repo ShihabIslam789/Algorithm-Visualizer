bdc5c00b4fb5d4a2257cc1622f8e051a09fa321e
Shihab Islam <shihabhunterislam@gmail.com>
2022-11-05 13:06:56 -0400

    stack checkled on grid.

--- pathvisual/src/algorithms/dfs.js
+++ pathvisual/src/algorithms/dfs.js
@@ -3,3 +3,12 @@ import * as utils from "./algorithmUtils.js";
 export default function dfs(grid, startNode, finishNode) {
   const visitedNodesInOrder = [];
   const nodeStack = [];
+  nodeStack.push(startNode);
+  while (!!nodeStack.length) {
+    const currentNode = nodeStack.pop();
+    if (currentNode.isWall) continue;
+    visitedNodesInOrder.push(currentNode);
+    if (currentNode === finishNode) return visitedNodesInOrder;
+    if (currentNode.isVisited === false) {
+      currentNode.isVisited = true;
+      const neighbors = utils.getNeighbors(currentNode, grid);
