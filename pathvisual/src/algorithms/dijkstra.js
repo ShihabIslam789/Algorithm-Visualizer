b05a6d54667f4e599b94cc733457283e4c7dbfd4
Shihab Islam <shihabhunterislam@gmail.com>
2022-11-07 19:28:56 -0500

    shift through unsorted nodes of graph.

--- pathvisual/src/algorithms/dijkstra.js
+++ pathvisual/src/algorithms/dijkstra.js
@@ -3,3 +3,6 @@ import * as utils from "./algorithmUtils.js";
 export default function dijkstra(grid, startNode, finishNode) {
   const visitedNodesInOrder = [];
   const unvisitedNodes = utils.getAllNodes(grid);
+  startNode.distance = 0;
+  while (!!unvisitedNodes.length) {
+    const minNode = utils.sortedNodes(unvisitedNodes).shift();
