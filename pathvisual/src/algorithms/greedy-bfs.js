79da0699e4537e24d0487fb18f13ca75f202a9d7
Shihab Islam <shihabhunterislam@gmail.com>
2022-11-08 19:51:25 -0500

    while loop for continue

--- pathvisual/src/algorithms/greedy-bfs.js
+++ pathvisual/src/algorithms/greedy-bfs.js
@@ -3,3 +3,7 @@ import * as utils from "./algorithmUtils.js";
 export default function greedyBfs(grid, startNode, finishNode, heuristic) {
     const visitedNodesInOrder = [];
     const nodePQ = [];
+    nodePQ.push(startNode);
+  while (!!nodePQ.length) {
+    const currentNode = nodePQ.shift();
+    if (currentNode.isWall) continue;
