29c55b20b21b12b174e88f09c04e4ebe572f772c
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2022-11-12 14:28:26 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import OptionBars from "./components/OptionBars";
+
+function App() {
+  return (
+    <div className='App'>
+      <OptionBars />
+    </div>
+  );
+}
+
+export default App;
